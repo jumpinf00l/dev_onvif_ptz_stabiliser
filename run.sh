a251aa20c9343481de7bec0ca1528e996b52947d
jumpinf00l <45162465+jumpinf00l@users.noreply.github.com>
2026-02-28 09:14:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# ==============================================================================
-# Home Assistant Add-on: ONVIF PTZ Stabiliser
-# ==============================================================================
 
-echo "Starting ONVIF PTZ Monitor..."
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S.%3N")
+
+echo "$TIMESTAMP - Starting ONVIF PTZ Stabiliser..."
 exec python3 -u /app/ptz_monitor.py
