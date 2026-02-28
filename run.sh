596d0d871fced257275e9e795113bb9a1bc401ec
jumpinf00l <45162465+jumpinf00l@users.noreply.github.com>
2026-02-28 09:07:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/with-contenv bash
+#!/bin/sh
 # ==============================================================================
 # Home Assistant Add-on: ONVIF PTZ Stabiliser
 # ==============================================================================
-set -e
 
 echo "Starting ONVIF PTZ Monitor..."
 exec python3 -u /app/ptz_monitor.py
