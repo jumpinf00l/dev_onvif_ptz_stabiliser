f7f82718aff01e2c3763d1aef2423038d9990b54
jumpinf00l <45162465+jumpinf00l@users.noreply.github.com>
2026-03-01 08:26:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S.%3N")
 
-echo "$TIMESTAMP - Starting ONVIF PTZ Stabiliser..."
-exec python3 -u /app/ptz_monitor.py
+echo "$TIMESTAMP - Starting ONVIF PTZ Helper..."
+exec python3 -u /app/onvif_ptz_helper.py
